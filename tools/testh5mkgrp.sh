704b4f4be300b44fe083f59f3cc377f1218366a1
Dana Robinson <derobins@hdfgroup.org>
2020-05-04 12:32:58 -0700

    Updated the h5mkgrp tests to be VOL-independent.

--- tools/testh5mkgrp.sh
+++ tools/testh5mkgrp.sh
@@ -21,7 +21,7 @@ TESTNAME=h5mkgrp
 EXIT_SUCCESS=0
 EXIT_FAILURE=1
 
-H5MKGRP=h5mkgrp             # The tool name
+H5MKGRP=h5mkgrp             # The h5mkgrp tool name
 H5LS=h5ls                   # The h5ls tool name
 H5LS_ARGS=-vr               # Arguments to the h5ls tool
 H5DELETE='h5delete -f'      # The h5delete tool name
@@ -37,10 +37,14 @@ AWK='awk'
 nerrors=0
 verbose=yes
 
+# Where the tool's input .h5 and "known good" output files are located
 H5MKGRP_TESTFILES="./testfiles"
 
-TESTDIR=./test_h5mkgrp
-test -d $TESTDIR || mkdir -p $TESTDIR
+# Where the text output from a tool or h5dump/h5ls/h5diff goes
+TEXT_OUTPUT_DIR=./h5mkgrp_test_output
+
+# Where the HDF5 output from the tool goes.
+HDF5_OUTPUT_DIR=./h5mkgrp_test_hdf5
 
 ######################################################################
 # test files
@@ -52,10 +56,9 @@ test -d $TESTDIR || mkdir -p $TESTDIR
 #       Comment '#' without space can be used.
 # --------------------------------------------------------------------
 
+# Expected output files.
 #
-# copy test files and expected output files from source dirs to test dir
-#
-COPY_TESTFILES="
+GOOD_OUTPUT_FILES="
 $H5MKGRP_TESTFILES/h5mkgrp_help.txt
 $H5MKGRP_TESTFILES/h5mkgrp_single.ls
 $H5MKGRP_TESTFILES/h5mkgrp_single_v.ls
@@ -71,25 +74,48 @@ $H5MKGRP_TESTFILES/h5mkgrp_nested_mult_p.ls
 $H5MKGRP_TESTFILES/h5mkgrp_nested_mult_lp.ls
 "
 
-COPY_TESTFILES_TO_TESTDIR()
+# Generated HDF5 files.
+#
+# This list is needed since we can't simply delete a directory with some
+# VOL connectors.
+#
+HDF5_OUTPUT_FILES="
+$HDF5_OUTPUT_DIR/h5mkgrp_single.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_single_v.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_single_p.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_single_l.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_several.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_several_v.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_several_p.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_several_l.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_nested_p.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_nested_lp.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_nested_mult_p.h5
+$HDF5_OUTPUT_DIR/h5mkgrp_nested_mult_lp.h5
+"
+
+# Copy the expected/"known good" text output files to the text output
+# directory.
+#
+COPY_GOOD_OUTPUT_FILES()
 {
-    # copy test files. Used -f to make sure get a new copy
-    for tstfile in $COPY_TESTFILES
+    # copy the 'known good' output files. Used -f to make sure get a new copy
+    for outfile in $GOOD_OUTPUT_FILES
     do
         # ignore '#' comment
-        echo $tstfile | tr -d ' ' | grep '^#' > /dev/null
+        echo $outfile | tr -d ' ' | grep '^#' > /dev/null
         RET=$?
         if [ $RET -eq 1 ]; then
             # skip cp if srcdir is same as destdir
             # this occurs when build/test performed in source dir and
             # make cp fail
-            SDIR=`$DIRNAME $tstfile`
+            SDIR=`$DIRNAME $outfile`
             INODE_SDIR=`$LS -i -d $SDIR | $AWK -F' ' '{print $1}'`
-            INODE_DDIR=`$LS -i -d $TESTDIR | $AWK -F' ' '{print $1}'`
+            INODE_DDIR=`$LS -i -d $TEXT_OUTPUT_DIR | $AWK -F' ' '{print $1}'`
             if [ "$INODE_SDIR" != "$INODE_DDIR" ]; then
-                $CP -f $tstfile $TESTDIR
+                $CP -f $outfile $TEXT_OUTPUT_DIR
                 if [ $? -ne 0 ]; then
-                    echo "Error: FAILED to copy $tstfile ."
+                    echo "Error: FAILED to copy output file: $outfile ."
 
                     # Comment out this to CREATE expected file
                     exit $EXIT_FAILURE
@@ -99,21 +125,34 @@ COPY_TESTFILES_TO_TESTDIR()
     done
 }
 
-CLEAN_TESTFILES_AND_TESTDIR()
+# Cleans up HDF5 and text output from the tests. Only cleans if the
+# HDF5_NOCLEANUP variable is unset.
+#
+CLEAN_OUTPUT()
 {
-    # skip rm if srcdir is same as destdir
-    # this occurs when build/test performed in source dir and
-    # make cp fail
-    SDIR=$H5MKGRP_TESTFILES
-    INODE_SDIR=`$LS -i -d $SDIR | $AWK -F' ' '{print $1}'`
-    INODE_DDIR=`$LS -i -d $TESTDIR | $AWK -F' ' '{print $1}'`
-    if [ "$INODE_SDIR" != "$INODE_DDIR" ]; then
-        $RM $TESTDIR
-    fi
+    # Remove output if the "no cleanup" environment variable is not defined
+    if test -z "$HDF5_NOCLEANUP"; then
+        # Text output
+        $RM $TEXT_OUTPUT_DIR
+
+        # HDF5 output
+        #
+        # Can't just rm -rf the directory if the VOL storage doesn't map to
+        # a normal file, so we'll use h5delete to delete the file.
+        for hdf5file in $HDF5_OUTPUT_FILES
+        do
+            $H5DELETE $hdf5file
+        done
+
+        # The HDF5 output directory is always created, even if the VOL
+        # storage won't use it. Delete it here.
+        $RM $HDF5_OUTPUT_DIR
+   fi
 }
 
 # Print a line-line message left justified in a field of 70 characters
 # beginning with the word "Testing".
+#
 TESTING()
 {
     SPACES="                                                               "
@@ -136,79 +175,80 @@ VERIFY_H5LS()
 # with exit status 0, consider it pass. If a test fails then increment
 # the `nerrors' global variable.
 # Assumed arguments:
-# $* arguments for h5mkgrp.
-
-TOOLTEST()
+#   $* arguments for h5mkgrp.
+#
+RUNTOOL()
 {
     TESTING $H5MKGRP $@
     (
-    cd $TESTDIR
-    $RUNSERIAL $H5MKGRP $@
-    ) > output.out
+    $H5MKGRP $@
+    ) > $TEXT_OUTPUT_DIR/output.out
     RET=$?
     if [ $RET != 0 ]; then
         echo "*FAILED*"
         echo "failed result is:"
-        cat output.out
+        cat $TEXT_OUTPUT_DIR/output.out
         nerrors="`expr $nerrors + 1`"
     else
         echo " PASSED"
 
         # Clean up output file
         if test -z "$HDF5_NOCLEANUP"; then
-           rm -f output.out
+           $RM $TEXT_OUTPUT_DIR/output.out
         fi
     fi
 }
 
 # Call the h5ls tool to verify the correct output data in the destination file
 #
-H5LSTEST()
+RUNH5LS()
 {
-    expect="$TESTDIR/`basename $1 .h5`.ls"
-    actual="$TESTDIR/`basename $1 .h5`.out"
+    expect="$TEXT_OUTPUT_DIR/`basename $1 .h5`.ls"
+    actual="$TEXT_OUTPUT_DIR/`basename $1 .h5`.out"
     actual_sav=${actual}-sav
 
-    # Stderr is included in stdout so that the diff can detect
+    # stderr is included in stdout so that the diff can detect
     # any unexpected output from that stream too.
     VERIFY_H5LS  $@
     (
-      cd $TESTDIR
-      $RUNSERIAL $H5LS $H5LS_ARGS $@
+      $H5LS $H5LS_ARGS $@
     ) 2>&1 |sed 's/Modified:.*/Modified:  XXXX-XX-XX XX:XX:XX XXX/' >$actual
 
-    # save actual in case it is needed later.
+    # Save actual in case it is needed later.
     cp $actual $actual_sav
+
+    # Filter output for things like MPI cruft
     STDOUT_FILTER $actual
     STDERR_FILTER $actual
 
-   if [ ! -f $expect ]; then
-      # Create the expect file if it doesn't yet exist.
-      echo " CREATED"
-      cp $actual $expect
-      echo "    Expected result (*.ls) missing"
-      nerrors="`expr $nerrors + 1`"
-   elif $CMP $expect $actual; then
-      echo " PASSED"
-   else
-      echo "*FAILED*"
-      echo "    Expected result (*.ls) differs from actual result (*.out)"
-      nerrors="`expr $nerrors + 1`"
-      test yes = "$verbose" && $DIFF $expect $actual |sed 's/^/    /'
-   fi
+    # Strip the HDF5 output directory name from the output file
+    # (use | to avoid sed and directory delimiter clash)
+    cp $actual $tmp_file
+    sed "s|$HDF5_OUTPUT_DIR/||" < $tmp_file > $actual
 
-   # Clean up output file
-   if test -z "$HDF5_NOCLEANUP"; then
-      rm -f $actual $actual_sav
-   fi
+    if [ ! -f $expect ]; then
+        # Create the expect file if it doesn't yet exist.
+        echo " CREATED"
+        cp $actual $expect
+        echo "    Expected result (*.ls) missing"
+        nerrors="`expr $nerrors + 1`"
+    elif $CMP $expect $actual; then
+        echo " PASSED"
+    else
+        echo "*FAILED*"
+        echo "    Expected result (*.ls) differs from actual result (*.out)"
+        nerrors="`expr $nerrors + 1`"
+        test yes = "$verbose" && $DIFF $expect $actual |sed 's/^/    /'
+    fi
 }
 
 # Single run of tool
 #
 # Assumed arguments:
-# $1 is test file name
-# $2 is h5mkgrp options
-# $* are groups to create
+#   $1 is test file name
+#   $2 is h5mkgrp options
+#   $* are groups to create
+#
 RUNTEST()
 {
     FILEOUT=$1
@@ -216,42 +256,33 @@ RUNTEST()
     H5MKGRP_ARGS=$1
     shift
 
-    # Remove any output file left over from previous test run
-    $H5DELETE $FILEOUT
-
     # Run test
-    TOOLTEST $H5MKGRP_ARGS $FILEOUT $@
+    RUNTOOL $H5MKGRP_ARGS $FILEOUT $@
 
     # Verify that the file created above is correct
-    H5LSTEST $FILEOUT
-
-    # Remove output file created, if the "no cleanup" environment variable is
-    #   not defined
-    if test -z "$HDF5_NOCLEANUP"; then
-        rm -f $TESTDIR/$FILEOUT
-    fi
+    RUNH5LS $FILEOUT
 }
 
 # Single run of tool
 #
 # Assumed arguments:
-# $1 is test expected output file
-# $2 is h5mkgrp options
-# $* are groups to create
+#   $1 is test expected output file
+#   $2 is h5mkgrp options
+#   $* are groups to create
+#
 CMPTEST()
 {
     FILEOUT=$1
-    expect="$TESTDIR/`basename $1 .h5`.txt"
-    actual="$TESTDIR/`basename $1 .h5`.out"
-    actual_err="$TESTDIR/`basename $1 .h5`.err"
+    expect="$TEXT_OUTPUT_DIR/`basename $1 .h5`.txt"
+    actual="$TEXT_OUTPUT_DIR/`basename $1 .h5`.out"
+    actual_err="$TEXT_OUTPUT_DIR/`basename $1 .h5`.err"
     shift
 
     # Stderr is included in stdout so that the diff can detect
     # any unexpected output from that stream too.
     TESTING $H5MKGRP $@
     (
-    cd $TESTDIR
-    $RUNSERIAL $H5MKGRP $@
+    $H5MKGRP $@
     ) >$actual 2>$actual_err
     cat $actual_err >> $actual
 
@@ -269,42 +300,44 @@ CMPTEST()
       nerrors="`expr $nerrors + 1`"
       test yes = "$verbose" && $DIFF $expect $actual |sed 's/^/    /'
    fi
-
-   # Clean up output file
-   if test -z "$HDF5_NOCLEANUP"; then
-      rm -f $actual $actual_err
-   fi
 }
 
 ##############################################################################
 ###           T H E   T E S T S                                            ###
 ##############################################################################
-# prepare for test
-COPY_TESTFILES_TO_TESTDIR
+# Prepare for test
+#
+# We create the HDF5 output dir in case it's needed. If a VOL connector doesn't
+# use normal files, it'll just stay empty and get deleted later.
+CLEAN_OUTPUT
+test -d $TEXT_OUTPUT_DIR || mkdir -p $TEXT_OUTPUT_DIR
+test -d $HDF5_OUTPUT_DIR || mkdir -p $HDF5_OUTPUT_DIR
+COPY_GOOD_OUTPUT_FILES
 
-# Check that help & version is displayed properly
+# Check that help is displayed properly
+# The file name is a dummy and isn't created (an artifact from other scripts)
 CMPTEST h5mkgrp_help.h5 "-h"
 
 # Create single group at root level
-RUNTEST h5mkgrp_single.h5 " " single
-RUNTEST h5mkgrp_single_v.h5 "-v" single
-RUNTEST h5mkgrp_single_p.h5 "-p" single
-RUNTEST h5mkgrp_single_l.h5 "-l" latest
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_single.h5 " " single
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_single_v.h5 "-v" single
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_single_p.h5 "-p" single
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_single_l.h5 "-l" latest
 
 # Create several groups at root level
-RUNTEST h5mkgrp_several.h5 " " one two
-RUNTEST h5mkgrp_several_v.h5 "-v" one two
-RUNTEST h5mkgrp_several_p.h5 "-p" one two
-RUNTEST h5mkgrp_several_l.h5 "-l" one two
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_several.h5 " " one two
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_several_v.h5 "-v" one two
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_several_p.h5 "-p" one two
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_several_l.h5 "-l" one two
 
 # Create various nested groups
-RUNTEST h5mkgrp_nested_p.h5 "-p" /one/two
-RUNTEST h5mkgrp_nested_lp.h5 "-lp" /one/two
-RUNTEST h5mkgrp_nested_mult_p.h5 "-p" /one/two /three/four
-RUNTEST h5mkgrp_nested_mult_lp.h5 "-lp" /one/two /three/four
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_nested_p.h5 "-p" /one/two
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_nested_lp.h5 "-lp" /one/two
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_nested_mult_p.h5 "-p" /one/two /three/four
+RUNTEST $HDF5_OUTPUT_DIR/h5mkgrp_nested_mult_lp.h5 "-lp" /one/two /three/four
 
-# Clean up temporary files/directories
-CLEAN_TESTFILES_AND_TESTDIR
+# Clean up generated files/directories
+CLEAN_OUTPUT
 
 if test $nerrors -eq 0 ; then
     echo "All $TESTNAME tests passed."
