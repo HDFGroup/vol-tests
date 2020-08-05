31660ab19352049cf59f98b1fba498b3e93dade5
Jordan Henderson <jhenderson@hdfgroup.org>
2020-08-05 14:40:57 -0500

    Add h5_szip_can_encode routine to tmisc.c for SZIP testing

--- hdf5_test/tmisc.c
+++ hdf5_test/tmisc.c
@@ -323,6 +323,51 @@ typedef struct
 /* and bad offset values are written to that file for testing */
 #define MISC33_FILE             "bad_offset.h5"
 
+#ifdef H5_HAVE_FILTER_SZIP
+/*-------------------------------------------------------------------------
+ * Function:  h5_szip_can_encode
+ *
+ * Purpose:  Retrieve the filter config flags for szip, tell if
+ *              encoder is available.
+ *
+ * Return:  1:  decode+encode is enabled
+ *    0:  only decode is enabled
+ *              -1: other
+ *
+ * Programmer:
+ *
+ * Modifications:
+ *
+ *-------------------------------------------------------------------------
+ */
+int h5_szip_can_encode(void)
+{
+    unsigned int filter_config_flags;
+
+    H5Zget_filter_info(H5Z_FILTER_SZIP, &filter_config_flags);
+    if ((filter_config_flags &
+            (H5Z_FILTER_CONFIG_ENCODE_ENABLED|H5Z_FILTER_CONFIG_DECODE_ENABLED)) == 0) {
+        /* filter present but neither encode nor decode is supported (???) */
+        return -1;
+    } else if ((filter_config_flags &
+            (H5Z_FILTER_CONFIG_ENCODE_ENABLED|H5Z_FILTER_CONFIG_DECODE_ENABLED)) ==
+            H5Z_FILTER_CONFIG_DECODE_ENABLED) {
+        /* decoder only: read but not write */
+        return 0;
+    } else if ((filter_config_flags &
+            (H5Z_FILTER_CONFIG_ENCODE_ENABLED|H5Z_FILTER_CONFIG_DECODE_ENABLED)) ==
+            H5Z_FILTER_CONFIG_ENCODE_ENABLED) {
+        /* encoder only: write but not read (???) */
+        return -1;
+    } else if ((filter_config_flags &
+            (H5Z_FILTER_CONFIG_ENCODE_ENABLED|H5Z_FILTER_CONFIG_DECODE_ENABLED)) ==
+            (H5Z_FILTER_CONFIG_ENCODE_ENABLED|H5Z_FILTER_CONFIG_DECODE_ENABLED)) {
+        return 1;
+    }
+   return(-1);
+}
+#endif /* H5_HAVE_FILTER_SZIP */
+
 /****************************************************************
 **
 **  test_misc1(): test unlinking a dataset from a group and immediately
